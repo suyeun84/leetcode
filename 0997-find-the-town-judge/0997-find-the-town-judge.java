282c47d79e175b9abae1976159712b7eb2cf8e6b
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2023-01-23 18:57:58 +0900

    Time: 1 ms (100.00%), Space: 49.6 MB (97.44%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -2,19 +2,16 @@ class Solution {
     public int findJudge(int n, int[][] trust) {
         
         int[] person = new int[n + 1];
-        boolean[] doTrust = new boolean[n + 1];
         
         for(int[] t : trust){
             int p = t[1];
-            int d = t[0];
             person[p]++;
-            doTrust[d] = true;
         }
         
         int a = 0;
         int judge = 0;
         for(int i = 1; i <= n; i++){
-            if(person[i] == n-1 && doTrust[i] == false){
+            if(person[i] == n-1){
                 a++;
                 judge = i;
             }
@@ -22,10 +19,10 @@ class Solution {
         }
         if(a == 0) return -1;
         
-        // for(int[] t : trust){
-        //     if(t[0] == judge) // judge는 아무도 안믿음
-        //         return -1;
-        // }
+        for(int[] t : trust){
+            if(t[0] == judge) // judge는 아무도 안믿음
+                return -1;
+        }
         
         return judge;
     }
