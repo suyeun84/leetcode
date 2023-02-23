272cb7fcb9ddc655cd629c28a23c669293c42108
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2023-02-23 23:35:45 +0900

    Time: 112 ms (48.77%), Space: 61.5 MB (63.76%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,42 +1,21 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        
-        int result = 0;
-        int[][] total = new int[profits.length][2];
-        int currK = 0;
 
-        //capital 순
-        PriorityQueue <int []> pq = new PriorityQueue<>(new Comparator<int []>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-                if(o1[0] == o2[0]) return o2[1] - o1[1];
-				else return o1[0]-o2[0];
-			}
-		});
-        //profit 순
+        PriorityQueue <int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
         PriorityQueue <Integer> maxProfit = new PriorityQueue<>(Collections.reverseOrder());
         
         for(int i = 0; i < profits.length; i++){
-            total[i][0] = capital[i];
-            total[i][1] = profits[i];
-            
-            pq.add(total[i]);
+            pq.add(new int[]{capital[i], profits[i]});
         }
 
-       while(true){
-           while(!pq.isEmpty() && pq.peek()[0] <= w){
-               maxProfit.add(pq.poll()[1]);
-           }
-           if(maxProfit.isEmpty())
-               return w;
-           
-           if(currK < k){
-               w += maxProfit.poll();
-               currK++;
-           }else{
-               return w;
-           }
-       }
-
+        for(int i = 0; i < k; i++){
+            while(!pq.isEmpty() && pq.peek()[0] <= w){
+                maxProfit.add(pq.poll()[1]);
+            }
+            if(maxProfit.isEmpty())
+                break;
+            w += maxProfit.poll();
+        }
+        return w;
     }
 }
